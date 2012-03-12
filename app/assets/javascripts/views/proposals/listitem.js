d9d7d690c55a291e94203b2062712406db0ad194
Nidhi Krishen <nidhi@corp.(none)>
2012-03-12 09:26:05 -0400

    new files, forgot to add in last commit, oops

---
+++ app/assets/javascripts/views/proposals/listitem.js
@@ -0,0 +1,25 @@
+//Post create request to back end for each specified nsf id
+App.Views.ListItemProposal = Backbone.View.extend({
+	tagName: 'tr',
+	render: function() {
+		var proposal = this.model.read();
+console.log(proposal);		
+		var compiled = _.template($('#template_proposals_listitem').html());
+		var data = {};
+		data["id"] = proposal.id;
+		data["nsf_id"] = proposal.nsf_id;
+		data["title"] = proposal.details.title;
+		data["date"] = proposal.details.status.date;
+		if (proposal.details.status.name=="award") data["amount"] = proposal.details.awarded.dollar; else data["amount"] = "(declined)";			
+		data["lastviewed"] = proposal.lastviewed;
+		var tmp = [];
+		if (proposal.panels && proposal.panels.length>0) {
+			_.each(proposal.panels, function(panel) {
+				tmp.push(panel["nsf_id"]+' - '+panel["name"]+' ('+panel["officer"]+')');
+			});
+		}
+		data["panels"] = tmp.join('<br />');
+		$(this.el).html(compiled(data));
+		return this;
+	}
+});
