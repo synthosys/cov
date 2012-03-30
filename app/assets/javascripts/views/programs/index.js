eb7b04ec400d44cbd50020e84cc03c639fe8dff3
Amy Y <amy.yu@synthosys.com>
2012-03-30 01:04:10 -0400

    some clean-up and added todo comment

--- app/assets/javascripts/views/programs/index.js
+++ app/assets/javascripts/views/programs/index.js
@@ -9,11 +9,20 @@ App.Views.IndexProgram = Backbone.View.extend({
                	// lookup using pge legend in the data api
                	$.getJSON("http://rd-dashboard.nitrd.gov/gapi/api/prop?legend=nsf_pge&q="+pges+"&jsoncallback=?", function(data){
 			var pgeList = data;
+			console.log(pgeList[pgeList.length-1])
 			var pgeTableTemplate = _.template($('#pgeTableTemplate').text());
 			var html = pgeTableTemplate({'pgeList' : pgeList});
 				$("#pge-list").html(html);
                        	});
-               	});        
+               	});    
+		/*function label(p){
+			if(p.full_label != null){
+				return p.full_label;
+			}
+			else{
+				return p.label;
+			}
+		}*/   
        	}
 });
 
