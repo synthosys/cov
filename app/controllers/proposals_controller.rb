30c0122d54e66a252518d6c9b536a7b320e7bc29
Ron Lai <laironald@gmail.com>
2012-04-01 22:05:48 -0400

    pre-production ron changes

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -34,7 +34,6 @@ class ProposalsController < ApplicationController
   # GET /proposals/1.json
   def show
     @proposal = Proposal.find(params[:id])
-    #set title
     tmp = ActiveSupport::JSON.decode(@proposal.details)
     @proposal_nsf_id = tmp['nsf_id']
     @proposal_title = tmp['title']
@@ -43,8 +42,6 @@ class ProposalsController < ApplicationController
     if current_user.role?:auditor then
       #find the association, update it
       associations = Association.where(:user_id => current_user.id, :proposal_id => @proposal.id)
-#Rails.logger.debug(association.inspect)
-#Rails.logger.flush()
       associations.each do |association|
         association.update_attribute(:lastviewed, Time.new) #update the last viewed attribute, this call skips validations so make sure whatever you're saving is legit
       end #in theory there should only be one found!
