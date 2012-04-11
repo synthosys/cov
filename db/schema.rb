3be1cea3263e223c985e5be12d5be0eb3bdf8afd
Ron Lai <laironald@gmail.com>
2012-04-11 16:24:43 -0400

    slightly modified schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,42 +14,47 @@
 ActiveRecord::Schema.define(:version => 20120319202325) do
 
   create_table "associations", :force => true do |t|
-    t.text     "comments"
-    t.datetime "lastviewed"
     t.integer  "user_id"
     t.integer  "proposal_id"
+    t.text     "comments"
+    t.datetime "lastviewed"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "proposals", :force => true do |t|
-    t.text     "reviewerproposals"
     t.text     "nsf_id"
     t.text     "details"
     t.text     "topics"
     t.text     "researchers"
     t.text     "panels"
     t.text     "reviewers"
+    t.text     "reviewerproposals"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "division"
   end
 
   create_table "users", :force => true do |t|
-    t.string   "division"
-    t.string   "name"
-    t.string   "email"
-    t.string   "username"
-    t.string   "encrypted_password"
-    t.string   "role",                   :limit => 10
+    t.string   "name",                                 :default => "", :null => false
+    t.string   "email",                                :default => "", :null => false
+    t.string   "username",                             :default => "", :null => false
+    t.string   "encrypted_password",                   :default => "", :null => false
+    t.string   "role",                   :limit => 10, :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count"
+    t.integer  "sign_in_count",                        :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "division"
   end
+
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
 end
