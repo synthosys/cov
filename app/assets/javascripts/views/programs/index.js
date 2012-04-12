374adc13e6b95249ab31623529b25eeb8327236c
Amy Y <amy.yu@synthosys.com>
2012-04-12 16:37:26 -0400

    correcting merge issues with master

--- app/assets/javascripts/views/programs/index.js
+++ app/assets/javascripts/views/programs/index.js
@@ -3,32 +3,6 @@ App.Views.IndexProgram = Backbone.View.extend({
 		"click a[class=link_to_topics]": "gotoTopics"
 	},
 	initialize: function() {
-<<<<<<< HEAD
-		this.render();
-       	},
-       	render: function() {
-               	// get a list of pges
-		$.getJSON("http://rd-dashboard.nitrd.gov/gapi/api/topic?org=CMMI&summ=pge&jsoncallback=?", function(data) {
-               		var pges = _.pluck(data["data"], "pge").join();
-               	// lookup using pge legend in the data api
-               	$.getJSON("http://rd-dashboard.nitrd.gov/gapi/api/prop?legend=nsf_pge&q="+pges+"&jsoncallback=?", function(data){
-			var pgeList = data;
-			console.log(pgeList[pgeList.length-1])
-			var pgeTableTemplate = _.template($('#pgeTableTemplate').text());
-			var html = pgeTableTemplate({'pgeList' : pgeList});
-				$("#pge-list").html(html);
-                       	});
-               	});    
-		/*function label(p){
-			if(p.full_label != null){
-				return p.full_label;
-			}
-			else{
-				return p.label;
-			}
-		}*/   
-       	}
-=======
 		var self = this;
 		require(['text!templates/programs/index.html'], function(html) {
 			var template = _.template(html);
@@ -65,5 +39,4 @@ App.Views.IndexProgram = Backbone.View.extend({
             });
        	});        
    	}
->>>>>>> 08bfbb91f11b3d6456fc6ef64965c06885521702
 });
